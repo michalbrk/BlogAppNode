916d0cdf17c06e3d5ab379ceb6ae1ef7bcac91ee
michalbrk <brakowski.m@gmail.com>
2018-07-24 09:35:30 +0200

    moving files to the Github repo

---
+++ [-e]/app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// ***Implementation of the own middleware***
+app.use(function (req, res, next) {
+    if(req.query.id) {
+        //process ID and call next() afterwards
+    } else if(req.query.author) {
+        //same as with the ID
+    } else if(req.query.id && req.query.ref) {
+        //when ID and Ref are present
+    } else {
+        next();
+    }
+})
+
+app.get('/about', function(req, res, next) {
+    //code executed after the middleware functions
+    //that process the query path
+})
+
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
